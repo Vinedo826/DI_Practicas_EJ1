fffd68a8ce7786c02a275eac3a248bf24abea618
Víctor Bravo <115560570+Vinedo826@users.noreply.github.com>
2025-03-04 19:09:35 +0100

    Add files via upload

---
+++ Recuperacion_EJ1/src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module org.example.recuperacion_ej1 {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens org.example.recuperacion_ej1 to javafx.fxml;
+    exports org.example.recuperacion_ej1;
+}
